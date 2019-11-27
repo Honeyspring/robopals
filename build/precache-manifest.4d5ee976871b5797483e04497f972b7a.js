25d496e184b54c31935532039f9f0abdabaad774
Honeyspring <54894768+Honeyspring@users.noreply.github.com>
2019-11-27 10:11:28 +0100

    include build folder

---
+++ build/precache-manifest.4d5ee976871b5797483e04497f972b7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57187b7a8849e1bfdc26089711ca697c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65cdaf2c44044c84850",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e93706ab525d5326581d",
+    "url": "/static/css/main.9c249a23.chunk.css"
+  },
+  {
+    "revision": "b65cdaf2c44044c84850",
+    "url": "/static/js/2.79a0e78e.chunk.js"
+  },
+  {
+    "revision": "e93706ab525d5326581d",
+    "url": "/static/js/main.e15da40a.chunk.js"
+  },
+  {
+    "revision": "aef4bf632a344e83cf07",
+    "url": "/static/js/runtime-main.5084ddff.js"
+  },
+  {
+    "revision": "eeed5afb245dbf3b8945493db31c76d8",
+    "url": "/static/media/webfont.eeed5afb.woff"
+  }
+]);
